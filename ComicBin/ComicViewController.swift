a3d89bb8dac01489854c51a17c0ee48027f2c299
Jefferey Rigler <jeff@riglerdigital.com>
2016-11-26 15:43:47 -0800

    Finished adding code for a working camera..

--- ComicBin/ComicViewController.swift
+++ ComicBin/ComicViewController.swift
@@ -10,17 +10,33 @@ import UIKit
 
 class ComicViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    @IBOutlet weak var addupdatebutton: UIButton!
+    
     @IBOutlet weak var titleTextField: UITextField!
     
     @IBOutlet weak var comicImageView: UIImageView!
 
     var imagePicker = UIImagePickerController()
+    var comic : Comic? = nil
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         imagePicker.delegate = self
+        
+        if comic != nil {
+            comicImageView.image = UIImage(data: comic!.image as! Data)
+            titleTextField.text = comic!.title
+            
+            addupdatebutton.setTitle("Update", for: .normal)
+        } else {
+            deleteButton.isHidden = true
+        }
+        
+        
         
         // Do any additional setup after loading the view.
     }
@@ -48,20 +64,47 @@ class ComicViewController: UIViewController, UIImagePickerControllerDelegate, UI
     
 
     @IBAction func cameraTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .camera
+        
+        present(imagePicker, animated: true, completion: nil)
     }
 
     
     @IBAction func addTapped(_ sender: Any) {
         
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        if comic != nil {
+            comic!.title = titleTextField.text
+            comic!.image = UIImagePNGRepresentation(comicImageView.image!) as NSData?
+            
+        } else {
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            
+            let comic = Comic(context: context)
+            comic.title = titleTextField.text
+            comic.image = UIImagePNGRepresentation(comicImageView.image!) as NSData?
         
-        let comic = Comic(context: context)
-        comic.title = titleTextField.text
-        comic.image = UIImagePNGRepresentation(comicImageView.image!) as NSData?
+        }
+        
+        
         
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
         
         navigationController!.popViewController(animated: true)
+    }
+    
+    
+    @IBAction func deleteTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        context.delete(comic!)
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
